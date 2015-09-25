5ddde890cfc2abe6717084b7578847afc24ad2e1
chench <chench@128.138.189.228>
2015-09-25 15:34:26 -0600

    add J2EE maven project

---
+++ condensate/condensate-algorithm/src/main/java/org/condensate/algorithm/App.java
@@ -0,0 +1,13 @@
+package org.condensate.algorithm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
