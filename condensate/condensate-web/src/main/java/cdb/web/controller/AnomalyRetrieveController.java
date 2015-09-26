f8f365bc5aabe03e401fb3c90e69b2e1235a26cd
chench <chench@128.138.189.228>
2015-09-26 16:15:23 -0600

    add AnomalyDetection controller

---
+++ condensate/condensate-web/src/main/java/cdb/web/controller/AnomalyRetrieveController.java
@@ -0,0 +1,11 @@
+package cdb.web.controller;
+
+/**
+ * The controller to handle the url 
+ * 
+ * @author Chao Chen
+ * @version $Id: AnomalyRetrieveController.java, v 0.1 Sep 26, 2015 4:14:18 PM chench Exp $
+ */
+public class AnomalyRetrieveController {
+
+}
