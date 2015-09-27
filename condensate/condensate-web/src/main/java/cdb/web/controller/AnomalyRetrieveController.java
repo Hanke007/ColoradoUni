2a2345a00858aa1e35b921560d61d036681a7fb4
Chao Chen <ppiachen@gmail.com>
2015-09-27 09:52:31 -0600

    build basic framework

--- condensate/condensate-web/src/main/java/cdb/web/controller/AnomalyRetrieveController.java
+++ condensate/condensate-web/src/main/java/cdb/web/controller/AnomalyRetrieveController.java
@@ -1,11 +1,29 @@
 package cdb.web.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import cdb.web.vo.AnomalyVO;
+
 /**
  * The controller to handle the url 
  * 
  * @author Chao Chen
  * @version $Id: AnomalyRetrieveController.java, v 0.1 Sep 26, 2015 4:14:18 PM chench Exp $
  */
+@Controller
+@RequestMapping("/anomly/")
 public class AnomalyRetrieveController {
 
+    @RequestMapping(value = "{dsName}", method = RequestMethod.GET)
+    public @ResponseBody List<AnomalyVO> getAnomalies(@PathVariable String dsName) {
+        List<AnomalyVO> result = new ArrayList<AnomalyVO>();
+        return result;
+    }
 }
---
+++ condensate/condensate-web/src/main/java/cdb/web/vo/AnomalyVO.java
@@ -0,0 +1,98 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2015 All Rights Reserved.
+ */
+package cdb.web.vo;
+
+import java.util.Date;
+
+/**
+ * The POJO of Anomalies
+ * 
+ * @author chench
+ * @version $Id: AnomalyVO.java, v 0.1 Sep 26, 2015 5:00:22 PM chench Exp $
+ */
+public class AnomalyVO {
+
+    /** the timestamp of the anomaly*/
+    private Date   date;
+    /** the value of the anomaly*/
+    private double val;
+    /** the position of the anomaly*/
+    private double longi;
+    /** the position of the anomaly*/
+    private double lati;
+
+    /**
+     * Getter method for property <tt>date</tt>.
+     * 
+     * @return property value of date
+     */
+    public Date getDate() {
+        return date;
+    }
+
+    /**
+     * Setter method for property <tt>date</tt>.
+     * 
+     * @param date value to be assigned to property date
+     */
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    /**
+     * Getter method for property <tt>val</tt>.
+     * 
+     * @return property value of val
+     */
+    public double getVal() {
+        return val;
+    }
+
+    /**
+     * Setter method for property <tt>val</tt>.
+     * 
+     * @param val value to be assigned to property val
+     */
+    public void setVal(double val) {
+        this.val = val;
+    }
+
+    /**
+     * Getter method for property <tt>longi</tt>.
+     * 
+     * @return property value of longi
+     */
+    public double getLongi() {
+        return longi;
+    }
+
+    /**
+     * Setter method for property <tt>longi</tt>.
+     * 
+     * @param longi value to be assigned to property longi
+     */
+    public void setLongi(double longi) {
+        this.longi = longi;
+    }
+
+    /**
+     * Getter method for property <tt>lati</tt>.
+     * 
+     * @return property value of lati
+     */
+    public double getLati() {
+        return lati;
+    }
+
+    /**
+     * Setter method for property <tt>lati</tt>.
+     * 
+     * @param lati value to be assigned to property lati
+     */
+    public void setLati(double lati) {
+        this.lati = lati;
+    }
+
+}
