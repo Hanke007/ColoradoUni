9dc67f6319c24caaccb252d5c9f773c8166afe64
Chao <1333776@tongji.edu.cn>
2016-01-05 10:46:42 -0700

    generate image in batch

--- condensate/condensate-algorithm/src/test/java/cdb/exp/qc/analysis/ImageDisplayAnalysis.java
+++ condensate/condensate-algorithm/src/test/java/cdb/exp/qc/analysis/ImageDisplayAnalysis.java
@@ -1,9 +1,13 @@
 package cdb.exp.qc.analysis;
 
+import java.text.ParseException;
+import java.util.Date;
 import java.util.List;
 import java.util.Properties;
 
 import cdb.common.lang.ConfigureUtil;
+import cdb.common.lang.DateUtil;
+import cdb.common.lang.ExceptionUtil;
 import cdb.common.lang.ImageWUtil;
 import cdb.common.model.DenseMatrix;
 import cdb.common.model.RegionAnomalyInfoVO;
@@ -46,5 +50,33 @@ public class ImageDisplayAnalysis extends AbstractQcAnalysis {
         DenseMatrix dMatrix = dProc.read(BinFileConvntnUtil.fileSSMI(rootDir, taskId, freqId));
         ImageWUtil.plotRGBImageWithMask(dMatrix, rootDir + taskId + ".bmp", sMatrix,
             ImageWUtil.BMP_FORMAT);
+    }
+
+    protected static void batchGenImage(DatasetProc dProc) {
+        Properties properties = ConfigureUtil.read("src/test/resources/zConfigQC.properties");
+        String rootDir = properties.getProperty("DATA_ROOT_DIR");
+        String begDate = properties.getProperty("BEGIN_DATE");
+        String endDate = properties.getProperty("END_DATE");
+        String freqId = properties.getProperty("FREQ_ID");
+
+        try {
+            Date bDate = DateUtil.parse(begDate, DateUtil.WEB_FORMAT);
+            Date eDate = DateUtil.parse(endDate, DateUtil.WEB_FORMAT);
+
+            while (!bDate.after(eDate)) {
+                DenseMatrix dMatrix = dProc
+                    .read(BinFileConvntnUtil.fileAVHR(rootDir, begDate, freqId));
+                if (dMatrix != null) {
+                    ImageWUtil.plotGrayImage(dMatrix, rootDir + bDate + ".bmp",
+                        ImageWUtil.BMP_FORMAT);
+                }
+
+                bDate.setTime(bDate.getTime() + 24 * 60 * 60 * 1000);
+            }
+
+        } catch (ParseException e) {
+            ExceptionUtil.caught(e, "Date format crashed");
+        }
+
     }
 }
